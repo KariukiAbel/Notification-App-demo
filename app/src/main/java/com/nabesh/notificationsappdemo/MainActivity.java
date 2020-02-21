cf8ca5357f83db5a9e690f71a271bfe871f58e1d
KariukiAbel <abelmuriithi@gmail.com>
2020-02-21 12:16:44 +0300

    Added NotificationCompact.Builder and instanciated it.Doing the on click listener in the MainActivity.

--- app/src/main/java/com/nabesh/notificationsappdemo/MainActivity.java
+++ app/src/main/java/com/nabesh/notificationsappdemo/MainActivity.java
@@ -1,14 +1,24 @@
 package com.nabesh.notificationsappdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn;
+    NotificationCompat.Builder notification;
+    public static final int uniqueid = 4630;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        notification = new NotificationCompat.Builder(this);
+        notification.setAutoCancel(true);
+        btn.setOnClickListener();
     }
 }

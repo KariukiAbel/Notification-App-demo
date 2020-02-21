4aa6c9fb8699f24bc0a4217e27b2197a05528391
KariukiAbel <abelmuriithi@gmail.com>
2020-02-21 13:18:42 +0300

    Built the notification and done the PendingIntent and used the NotificationManager

--- app/src/main/java/com/nabesh/notificationsappdemo/MainActivity.java
+++ app/src/main/java/com/nabesh/notificationsappdemo/MainActivity.java
@@ -3,7 +3,11 @@ package com.nabesh.notificationsappdemo;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -19,6 +23,26 @@ public class MainActivity extends AppCompatActivity {
         btn = findViewById(R.id.btn);
         notification = new NotificationCompat.Builder(this);
         notification.setAutoCancel(true);
-        btn.setOnClickListener();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Building the notification
+                notification.setContentTitle("New notification");
+                notification.setContentText("Simple notification demo");
+                notification.setTicker("This is ticker");
+                notification.setWhen(System.currentTimeMillis());
+                Log.d("notification","Success");
+
+                Intent intent = new Intent(this,MainActivity.class);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+                notification.setContentIntent(pendingIntent);
+
+                //Builds notification and issues it
+                NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                notificationManager.notify(uniqueid, notification.build());
+
+
+            }
+        });
     }
 }
